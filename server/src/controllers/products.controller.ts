d160307dada55ff9867749fc9c4b71996e597bf4
Daniel <danielenrique.ecole42@gmail.com>
2026-09-01 13:22:46 +0200

    feat: add product lookup and search

--- server/src/controllers/products.controller.ts
+++ server/src/controllers/products.controller.ts
@@ -1,16 +1,52 @@
 import type { Request, Response } from 'express';
-import { getAllProducts } from '../services/products.service.js';
+import { getAllProducts, getProductById as getProductByIdService, searchProducts } from '../services/products.service.js';
 
 export async function getProducts(req: Request, res: Response) {
     try {
-        const products = await getAllProducts();
+        const search = req.query.search;
 
+        if (typeof search == 'string') {
+            const products = await searchProducts(search);
+            res.json(products);
+            return;
+        }
+
+        const products = await getAllProducts();
         res.json(products);
     } catch (error) {
         console.error(error);
 
         res.status(500).json({
             error: 'Failed to get products'
+        });
+    }
+}
+
+export async function getProductById(req: Request, res: Response) {
+    try {
+        const id = Number(req.params.id);
+        if (Number.isNaN(id)) {
+            res.status(400).json({
+                error: 'Id is NaN'
+            });
+            return;
+        }
+
+        const product = await getProductByIdService(id);
+        if (!product) {
+            res.status(404).json({
+                error: 'Product not found'
+            });
+
+            return;
+        }
+
+        res.json(product);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            error: 'Failed to get product'
         });
     }
 }
--- server/src/services/products.service.ts
+++ server/src/services/products.service.ts
@@ -15,3 +15,39 @@ export async function getAllProducts() {
 
     return result.rows;
 }
+
+export async function getProductById(id: number) {
+const result = await pool.query(`
+    SELECT
+        products.id,
+        products.name,
+        categories.name AS category,
+        products.stock,
+        products.stock_unit
+    FROM products
+    JOIN categories
+        ON categories.id = products.category_id
+    WHERE products.id = $1;
+`, [id]);
+
+    return result.rows[0];
+}
+
+export async function searchProducts(search: string) {
+    const pattern = `%${search}%`;
+
+    const result = await pool.query(`
+        SELECT
+            products.id,
+            products.name,
+            categories.name AS category,
+            products.stock,
+            products.stock_unit
+        FROM products
+        JOIN categories
+            ON categories.id = products.category_id
+        WHERE products.name ILIKE $1;
+    `, [pattern]);
+
+    return result.rows;
+}
