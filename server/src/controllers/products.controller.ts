d29b1133340d440934dc1fda44e8240ffcae4fee
Daniel <danielenrique.ecole42@gmail.com>
2026-09-23 10:45:54 +0200

    createProduct controller

--- server/src/controllers/products.controller.ts
+++ server/src/controllers/products.controller.ts
@@ -3,7 +3,9 @@ import {
   getAllProducts,
   getProductById as getProductByIdService,
   searchProducts,
+  createProduct as createProductService,
 } from "../services/products.service.js";
+import { DatabaseError } from "pg";
 
 export async function getProducts(req: Request, res: Response) {
   try {
@@ -56,6 +58,7 @@ export async function getProductById(req: Request, res: Response) {
 
 export async function createProduct(req: Request, res: Response) {
   try {
+    const stockValidations = ["BOX", "BAG", "BOTTLE", "UNIT"];
     const {
       name,
       category_id,
@@ -68,13 +71,72 @@ export async function createProduct(req: Request, res: Response) {
     if (
       name == undefined ||
       category_id == undefined ||
-      stock_unit ||
-      undefined
+      stock_unit == undefined
     ) {
       res.status(400).json({
         error: "Missing required fields",
       });
       return;
     }
-  } catch (error) {}
+
+    if (!stockValidations.includes(stock_unit)) {
+      res.status(400).json({
+        error: "Invalid stock unit",
+      });
+      return;
+    }
+
+    if (
+      stock !== undefined &&
+      (typeof stock !== "number" || !Number.isInteger(stock) || stock < 0)
+    ) {
+      res.status(400).json({
+        error: "Stock must be a non-negative number.",
+      });
+      return;
+    }
+
+    if (typeof name !== "string" || name.trim().length === 0) {
+      res.status(400).json({
+        error: "Name must be a non-empty string.",
+      });
+      return;
+    }
+
+    if (
+      typeof category_id !== "number" ||
+      !Number.isInteger(category_id) ||
+      category_id <= 0
+    ) {
+      res.status(400).json({
+        error: "category_id must be a positive integer.",
+      });
+      return;
+    }
+
+    const product = await createProductService({
+      name,
+      category_id,
+      stock,
+      stock_unit,
+      package_quantity,
+      package_unit,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    console.error(error);
+
+    // Handle foreign key violation
+    if (error instanceof DatabaseError && error.code === "23503") {
+      res.status(400).json({
+        error: "Category does not exist.",
+      });
+      return;
+    }
+
+    res.status(500).json({
+      error: "Failed to create product",
+    });
+  }
 }
