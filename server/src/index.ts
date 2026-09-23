4d22269dc569fc79e960123dc441c913df55a662
Daniel <danielenrique.ecole42@gmail.com>
2026-09-23 10:50:54 +0200

    add express.json for readable http request

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
 import express, { type Express, type Request, type Response } from "express";
-import { pool } from "./db.js";
 import productsRouter from "./routes/products.routes.js";
 
 const app: Express = express();
 const port = 3000;
 
+app.use(express.json());
 app.use("/products", productsRouter);
 
 app.listen(port, () => {
